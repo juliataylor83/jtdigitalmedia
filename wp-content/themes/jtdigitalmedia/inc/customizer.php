5630c9c6c6d6a00e8440692d9c4834192a963368
Julia Taylor <juliataylor83@gmail.com>
2016-11-14 10:16:18 -0500

    initial repo setup

---
+++ wp-content/themes/jtdigitalmedia/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * JTDigitalMedia Theme Customizer.
+ *
+ * @package JTDigitalMedia
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function jtdigitalmedia_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'jtdigitalmedia_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function jtdigitalmedia_customize_preview_js() {
+	wp_enqueue_script( 'jtdigitalmedia_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'jtdigitalmedia_customize_preview_js' );
